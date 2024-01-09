5bbb841dd79016feb0acc50fa8ed78cacfa179db
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-09 18:44:54 +0000

    Added Procfile and updated requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port="5000", host="0.0.0.0")
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
