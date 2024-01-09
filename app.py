4dbfcd08ca3dbffbe6b4e3b6c5ac36fd41dab9bd
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-09 19:44:16 +0000

    Renamed main.py back to app.py, added host parameter to app.run as '0.0.0.0' and removed Procfile

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from website import create_app
-import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port="5000", host="0.0.0.0")
