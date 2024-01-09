bb9b3635b63600aa9e69af756dc2f8d148257640
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-09 21:14:53 +0000

    Moved index route into app.py

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
 from website import create_app
-from flask import redirect, url_for, flash
+from flask import redirect, url_for, flash, render_template
+from website.utils import login_required, usd
+import base64
+import website
 
 app = create_app()
+
+# display homepage
+@app.route("/")
+@login_required
+def index():
+    
+    full_product_information = website.lib_stores_db.execute(
+        """
+            SELECT * FROM products 
+            ORDER BY RANDOM();
+        """
+    )
+    
+    product_id_and_photos = {}
+    
+    for product_info in full_product_information:
+        # format the price as USD ($29.93)
+        product_info['price'] = usd(product_info['price'])
+        
+        product_id_and_photos[product_info['id']] = website.lib_stores_db.execute(
+            """
+                SELECT picture FROM product_pictures
+                WHERE product_id = ?
+            """,
+            product_info['id']
+        )
+        
+    """
+        loop through the `full_product_information` and use the id to uniquely 
+        identify elements in the `product_id_and_info` dictionary that would have
+        a key that will be an id of a product and the value of the `product_id_and_info`
+        is an array that was returned by the SQL query which would be a list
+    """
+    
+    for product_info in full_product_information:
+        for info_and_id in product_id_and_photos[product_info['id']]:
+            # conver each picture BLOB that returned from the SQL query to a base 64 image
+            info_and_id['picture'] = base64.b64encode(info_and_id['picture']).decode("utf-8")
+        
+    
+    return render_template("index.html", full_product_information=full_product_information, product_id_and_photos=product_id_and_photos)
+
 
 @app.errorhandler(404)
 def page_not_found(code=404):
     flash(message=("404 - Page Not Found!", "You requested a page that does not exist on our server!"), category="danger")
-    return redirect(url_for("all_routes.index"))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(debug=True, port="5000", host="0.0.0.0")
