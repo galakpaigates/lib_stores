e412683f04b040a163aabe5f2f26ff6bd301af0d
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-10 14:04:41 +0000

    Modified project directory structure per vercel flask specifications

---
+++ app/app.py
@@ -0,0 +1,63 @@
+import os, base64
+from flask import redirect, url_for, flash, render_template, jsonify
+from utils import login_required, usd
+from __init__ import create_app, lib_stores_db
+
+app = create_app()
+
+# display homepage
+@app.route("/")
+@login_required
+def index():
+    
+    full_product_information = lib_stores_db.execute(
+        """
+            SELECT * FROM products
+            ORDER BY RANDOM();
+        """
+    )
+    
+    product_id_and_photos = {}
+    
+    for product_info in full_product_information:
+        # format the price as USD ($29.93)
+        product_info['price'] = usd(product_info['price'])
+        
+        product_id_and_photos[product_info['id']] = lib_stores_db.execute(
+            """
+                SELECT picture FROM product_pictures
+                WHERE product_id = ?
+            """,
+            product_info['id']
+        )
+        
+    """
+        loop through the `full_product_information` and use the id to uniquely 
+        identify elements in the `product_id_and_info` dictionary that would have
+        a key that will be an id of a product and the value of the `product_id_and_info`
+        is an array that was returned by the SQL query which would be a list
+    """
+    
+    for product_info in full_product_information:
+        for info_and_id in product_id_and_photos[product_info['id']]:
+            # conver each picture BLOB that returned from the SQL query to a base 64 image
+            info_and_id['picture'] = base64.b64encode(info_and_id['picture']).decode("utf-8")
+        
+    
+    return render_template("index.html", full_product_information=full_product_information, product_id_and_photos=product_id_and_photos)
+
+
+@app.errorhandler(404)
+def page_not_found(code=404):
+    flash(message=("404 - Page Not Found!", "You requested a page that does not exist on our server!"), category="danger")
+    return redirect(url_for("index"))
+
+
+@app.errorhandler(500)
+def page_not_found(code=500):
+    flash(message=("500 - Internal Server Error!", "There is a problem with our server, please refresh and try again!"), category="danger")
+    return None
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
---
+++ app/static/js/app.js
@@ -0,0 +1,156 @@
+// show and hide password
+try {
+    document.getElementById("show_passwords").addEventListener('click', () => 
+    {
+        document.querySelectorAll(".password_input").forEach((password_input) =>
+        {
+            password_input.type === "password" ? password_input.type = "text" : password_input.type = "password";
+        });
+    });
+} catch (error) {/* pass */}
+
+// display product picture preview
+try
+{
+    document.getElementById('customFile').addEventListener('change', (event) => 
+    {
+        if (event.target.files[0].type.startsWith("image/"))
+            document.getElementById("product_picture_preview_div").style.padding = "2em 0";
+
+        var selected_profile_picture;
+
+        for (let i = 0; i < event.target.files.length; i++)
+        {
+            selected_profile_picture = event.target.files[i]
+
+            if (selected_profile_picture.type.startsWith("image/"))
+            {
+
+                const read_file = new FileReader()
+                read_file.readAsDataURL(selected_profile_picture)
+
+                read_file.addEventListener('load', () =>
+                {
+                    document.getElementById('product_picture_preview_div').insertAdjacentHTML("beforeend", 
+                        `<img class="product_picture_preview" src="${read_file.result}" alt="Selected Image">`
+                    );
+                });
+            }
+        }
+    });
+} catch (error) { /* pass */}
+
+// display store profile picture preview
+try
+{
+    document.getElementById('customFile').addEventListener('change', (event) => 
+    {
+        const selected_profile_picture = event.target.files[0]
+
+        const read_file = new FileReader()
+        read_file.readAsDataURL(selected_profile_picture)
+
+        read_file.addEventListener('load', () =>
+        {
+            document.getElementById('profile_picture_preview_div').innerHTML = `<img id="profile_picture_preview" src="${read_file.result}" alt="Selected Image">`
+        });
+    })
+} catch (error) { /* pass */}
+
+// switch tabs on the store profile page
+try
+{
+    switch_tab(0)
+} catch (error) {/* pass */}
+
+document.addEventListener("click", (event) => 
+{
+    const clicked_element = event.target;
+
+    // listen for changing of tabs
+    if (clicked_element.className.includes("nav_bar_title"))
+    {
+        clicked_element.style.backgroundColor = '#193053';
+        clicked_element.style.color = '#e3e1e3';
+        
+        switch_tab(parseInt(clicked_element.dataset.tabindex));
+    }
+
+    else if (clicked_element.className === "next_pic")
+        next_picture(clicked_element)
+
+    else if (clicked_element.className === "prev_pic")
+        prev_picture(clicked_element)
+});
+
+// display and remove the contents of the list elements
+function switch_tab(idx = -1)
+{
+    // change style of the tab title to potray current tab
+    document.querySelectorAll(".nav_bar_title").forEach((button) =>
+    {
+        if (button.dataset.tabindex == idx)
+        {
+            button.style.color = "#e3e1e3";
+            button.style.backgroundColor = "#193053";
+        }
+        else
+        {
+            button.style.color = "#404940";
+            button.style.backgroundColor = "#e3e1e3";
+        }
+    })
+
+    // switch the tab
+    const all_tabs = document.getElementById('nav_tabs').querySelectorAll("li");
+
+    for (let i = 0; i < all_tabs.length; i++)
+    {
+        if (i != idx)
+            all_tabs[i].style.display = "none";
+        else
+            all_tabs[i].style.display = "flex";
+    }
+}
+
+// move to the next image in the slideshow of product_pictures
+function next_picture(particular_product)
+{
+    // change style of the tab title to potray current tab
+    const particular_each_pic_div = particular_product.parentElement.querySelector(".product_pictures_slideshow_div").querySelectorAll(".carousel-item");
+
+    for (let i = 0; i < particular_each_pic_div.length; i++)
+    {
+        if (particular_each_pic_div[i].classList.contains("active"))
+        {
+            particular_each_pic_div[i].classList.remove("active");
+
+            if (i == particular_each_pic_div.length-1)
+                particular_each_pic_div[0].classList.add("active")
+            else
+                particular_each_pic_div[i + 1].classList.add("active")
+            break;
+        }
+    }
+}
+
+// move to the previous image in the slideshow of product_pictures
+function prev_picture(particular_product)
+{
+    // change style of the tab title to potray current tab
+    const particular_each_pic_div = particular_product.parentElement.querySelector(".product_pictures_slideshow_div").querySelectorAll(".carousel-item");
+
+    for (let i = 0; i < particular_each_pic_div.length; i++)
+    {
+        if (particular_each_pic_div[i].classList.contains("active"))
+        {
+            particular_each_pic_div[i].classList.remove("active");
+
+            if (i == 0)
+                particular_each_pic_div[particular_each_pic_div.length - 1].classList.add("active")
+            else
+                particular_each_pic_div[i - 1].classList.add("active")
+            break;
+        }
+    }
+}
