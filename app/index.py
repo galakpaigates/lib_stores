f9ed873cf3a231be7877d916206bbc3f9efa2097
Jee-do Gbozee Arkoi <118209119+galakpaigates@users.noreply.github.com>
2024-01-10 17:41:23 +0000

    Update index.py

--- app/index.py
+++ app/index.py
@@ -77,5 +77,5 @@ def portfolio():
 def contact():
     return 'Contact Page Route'
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
