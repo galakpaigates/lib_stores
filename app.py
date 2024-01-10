8884db6ef121519325f65a339c1a468119f701c7
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-10 14:39:36 +0000

    moved everything in the  dir into the root dir

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def page_not_found(code=500):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
