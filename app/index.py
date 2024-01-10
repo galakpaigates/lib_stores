0f8c7a95f53d885bc64532d747564b6bf5eb0f89
Jee-do Gbozee Arkoi <118209119+galakpaigates@users.noreply.github.com>
2024-01-10 17:46:41 +0000

    Update index.py

--- app/index.py
+++ app/index.py
@@ -54,28 +54,15 @@
 
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
-    return 'Home Page Route'
-
+    return 'Hello, World!'
 
 @app.route('/about')
 def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
+    return 'About'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
