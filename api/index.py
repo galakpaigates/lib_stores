abccede7bd19749ce9ef0d26bf0b3e68bcf0eeb2
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-10 18:41:39 +0000

    commented __name__ == '__main__'

--- api/index.py
+++ api/index.py
@@ -53,5 +53,5 @@ def page_not_found(code=404):
     return redirect(url_for("index"))
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(port="5000", host="0.0.0.0")
