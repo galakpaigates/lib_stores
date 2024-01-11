be01c06a0946c6d61620d12235d03f4821206f3b
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-11 18:14:49 +0000

    fixed problem with UPLOAD_FOLDER having double /app

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,12 @@ def create_app():
     app.config['SECRET_KEY'] = "ADSFJ kjsd fadfasdfkJ FKAJSDFLKAJSFQ209843IU23J !@#$@#%kqewfdcx2POIEWUJ"
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
-
+    
     UPLOAD_FOLDER = os.path.join(app.root_path, 'static/imgs/tmp_profile')
+    
+    # ensusre there is no double /app at the beginning of the UPLOAD_FOLDER
+    if UPLOAD_FOLDER.startswith("/app"):
+        UPLOAD_FOLDER.replace("/app", "", 1)
 
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
