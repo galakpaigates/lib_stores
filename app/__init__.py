df78f0b43425b675576efc021a224a6e5a3e989c
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-12 12:33:31 +0000

    corrected error in .gitignore file that was ignoring the tmp_profile

--- app/__init__.py
+++ app/__init__.py
@@ -8,16 +8,11 @@ def create_app():
     
     app = Flask(__name__)
     
-    if app.root_path.endswith("/app"):
-        tmp_root_path = app.root_path.rsplit("ap", 1)[0]
-    
     app.config['SECRET_KEY'] = "ADSFJ kjsd fadfasdfkJ FKAJSDFLKAJSFQ209843IU23J !@#$@#%kqewfdcx2POIEWUJ"
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
-    print(app.root_path)
-    print(tmp_root_path)
-    UPLOAD_FOLDER = os.path.join(tmp_root_path, 'static/imgs/tmp_profile')
-    print(UPLOAD_FOLDER)
+    
+    UPLOAD_FOLDER = os.path.join(app.root_path, 'static/imgs/tmp_profile')
     
     # initialize upload folder
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
