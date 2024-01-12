1ef4bbb1977766e585baa43073d631ce55c90f32
galakpaigates <jeedoarkoi2006@gmail.com>
2024-01-12 11:34:18 +0000

    created a tmp_root_path to use to create UPLOAD_FOLDER path configuration

--- app/__init__.py
+++ app/__init__.py
@@ -6,12 +6,16 @@ lib_stores_db = SQL("sqlite:///lib_stores.db")
 
 def create_app():
     print(os.path)
-    app = Flask(__name__, root_path="/")
+    app = Flask(__name__)
+    
+    if app.root_path.endswith("/app"):
+        tmp_root_path = app.root_path.rsplit("ap", 1)[0]
+    
     app.config['SECRET_KEY'] = "ADSFJ kjsd fadfasdfkJ FKAJSDFLKAJSFQ209843IU23J !@#$@#%kqewfdcx2POIEWUJ"
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['MAX_CONTENT_LENGTH'] = 512 * 1024 * 1024
     
-    UPLOAD_FOLDER = os.path.join(app.root_path, 'app/', 'static/imgs/tmp_profile')
+    UPLOAD_FOLDER = os.path.join(tmp_root_path, 'app/', 'static/imgs/tmp_profile')
     
     # initialize upload folder
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
